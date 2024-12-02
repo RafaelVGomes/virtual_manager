2f86b58bea29475683e6d65ea3defaf981c97cfe
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-12-02 17:44:09 -0300

    LAYOUT: Changes on messages display to "toast".

--- virtual_manager/static/layout.js
+++ virtual_manager/static/layout.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Compares if the 3rd part of the visited link and <li> href link matches
-    const url_prefix = $('#index-tabs').length ? 3 : 4
-    const layoutNavbar = document.getElementById('layoutNavbar')
-    layoutNavbar.querySelectorAll(".nav-link").forEach((x) => {
-      if (window.location.href.split('/')[url_prefix] == x.href.split('/')[url_prefix]) {
-        x.classList.add('active', 'disabled')
-      }
-    })
+  // Makes nav items active and disabled if <li> href matches part of the url
+  const url_prefix = $('#index-tabs').length ? 3 : 4
+  const layoutNavbar = document.getElementById('layoutNavbar')
+  layoutNavbar.querySelectorAll(".nav-link").forEach((x) => {
+    if (window.location.href.split('/')[url_prefix] == x.href.split('/')[url_prefix]) {
+      x.classList.add('active', 'disabled')
+    }
+  })
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Inicializa o toast
+  const toastElement = document.getElementById('example-toast');
+  const toast = new bootstrap.Toast(toastElement, {
+    autohide: true, // Faz o toast desaparecer automaticamente
+    delay: 5000     // Define o tempo de exibição (5 segundos)
+  });
+
+  // Exibe o toast
+  toast.show();
+});
