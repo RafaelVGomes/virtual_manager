abf9b22f8ee8b8d30daa52d2445c35d45f73ded8
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-12-03 08:44:21 -0300

    INDEX: Remove 'cash' from code + minor changes

--- virtual_manager/src/index/view/index.py
+++ virtual_manager/src/index/view/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, render_template, request, g, url_for
+from flask import Blueprint, flash, render_template, g
 
 from virtual_manager.db import get_db
 from virtual_manager.src.auth.view.auth import login_required
@@ -9,12 +9,11 @@ bp = Blueprint('index', __name__, template_folder='../html')
 @bp.route("/")
 @login_required
 def index():
-  """Show portfolio of stocks"""
+  """Shows items and products in stock"""
   db = get_db()
   user_id = g.user['id']
-  cash = db.execute("SELECT cash FROM users WHERE id = ?;", (user_id,)).fetchone()['cash']
   items = db.execute("SELECT * FROM items WHERE user_id = ?;", (user_id,)).fetchall()
   products = db.execute("SELECT * FROM products WHERE user_id = ?;", (user_id,)).fetchall()
   # recipes = db.execute("SELECT * FROM recipes;").fetchall()
-  return render_template("index.html", cash=cash, items=items, products=products)
+  return render_template("index.html", items=items, products=products)
   
