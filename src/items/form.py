821ce3c49f0563916a7a91b46738ab8a76b956e7
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2024-12-17 18:44:31 -0300

    ITEMS: changed for headaches... never more... -.-'

---
+++ src/items/form.py
@@ -0,0 +1,128 @@
+from copy import deepcopy
+from sqlite3 import Row
+from typing import Literal, Optional, TypedDict, Union
+
+from flask import Request, abort, flash, g
+
+from ..db import get_db
+
+class Attrs(TypedDict, total=False):
+  min: str
+  step: str
+
+class FormField(TypedDict):
+  name: str
+  value: Union[str, int, float]
+  type: Optional[Literal['text', 'number']]
+  attrs: Optional[Attrs]
+
+FORM_DATA: list[FormField] = [
+  {
+  'name': 'item_name',
+  'value': '',
+  'type': 'text'
+  },
+  {
+    'name': 'amount',
+    'value': 0,
+    'type': 'number',
+    'attrs': {'min':"0"}
+  },
+  {
+    'name': 'measure',
+    'type': None,
+    'value': ''
+  },
+  {
+    'name': 'quantity_alert',
+    'value': 0,
+    'type': 'number',
+    'attrs': {'min':"0"}
+  },
+  {
+    'name': 'price',
+    'value': 0.0,
+    'type': 'number',
+    'attrs': {'step':"0.01", 'min':"0"}
+  }
+]
+
+def get_form_data(id: Optional[int]=None) -> list[FormField]:
+  db = get_db()
+  new_form = deepcopy(FORM_DATA)
+  
+  if id:
+    item = db.execute(
+      "SELECT * FROM items WHERE id = ? AND user_id = ?;", (id, g.user['id'])
+    ).fetchone()
+
+    if not item:
+      abort(404, description="Item not found or you do not have permission to access it.")
+    
+    item_dict = dict(item)
+    
+    for key, value in item_dict.items():
+      for field in new_form:
+        if field['name'] == key:
+          field['value'] = value
+    
+    # new_form.append({'id': id})
+  # print(f"{new_form=}")
+  return new_form
+
+def get_form(request: Request) -> list[FormField]:
+  id = request.view_args.get('id')
+  received_form = get_form_data()
+
+  for field in received_form:
+    if field['type'] == 'text':
+      field['value'] = request.form.get(field['name']).lower()
+    elif field['type'] == None:
+      field['value'] = request.form.get(field['name'])
+    elif field['type'] == 'number' and 'step' in field['attrs'].keys():
+      field['value'] = request.form.get(field['name'], type=float)
+    else:
+      field['value'] = request.form.get(field['name'], type=int)
+  
+  if id:
+    received_form.append({
+      'name': 'id',
+      'type': 'number',
+      'value': id
+    })
+  
+  return received_form
+
+def validate_form(form: list[FormField]) -> bool:
+  """Validate item form."""
+  errors = {}
+
+  for field in form:
+    if field['name'] == 'item_name' and not field['value']:
+      errors['item_name'] = "Please enter a name for this item."
+
+    if field['name'] == 'amount' and not (field['value'] or field['value'] <= 0):
+      errors['amount'] = "Amount must be greater than zero."
+
+    if field['name'] == 'measure' and not field['value']:
+      errors['measure'] = "Please select a measure."
+    elif field['name'] == 'measure' and field['value'] not in ['kg', 'L', 'pcs']:
+      errors['measure'] = "Invalid measure."
+
+    if field['name'] == 'quantity_alert' and not (field['value'] or field['value'] < 0):
+      errors['quantity_alert'] = "Quantity alert must be zero or greater."
+
+    # if field['name'] == 'price' and not field['value']:
+    #   errors['price'] = "Price must be zero or greater."
+
+  if errors:
+    flash('Fill all required fields!#warning', 'messages')
+    for field, message in errors.items():
+      flash(message, field)
+    return False
+  return True
+
+def get_data_to_save(form: list[FormField]) -> dict:
+  flat_form = {field['name']: field['value'] for field in form}
+  flat_form['user_id'] = g.user['id']
+  return flat_form
---
+++ src/items/views.py
@@ -0,0 +1,97 @@
+from flask import (Blueprint, abort, flash, g, redirect, render_template, request,
+                   url_for)
+
+from .form import get_data_to_save, get_form, get_form_data, validate_form
+from ..db import get_db
+from ..auth.view.auth import login_required
+
+
+bp = Blueprint('items', __name__, url_prefix='/items', template_folder='./html', static_folder='../../items')
+
+@bp.route("/overview")
+@login_required
+def overview():
+  """List all items in stock."""
+  db = get_db()
+  items = db.execute("SELECT * FROM items WHERE user_id = ? ORDER BY items.item_name ASC;", (g.user['id'],)).fetchall()
+  return render_template("items.html", items=items)
+
+@bp.route("/create-item", methods=["GET", "POST"])
+@login_required
+def create_item():
+  """Create item."""
+  form = get_form_data()
+
+  if request.method == "POST":
+    form = get_form(request)
+    
+    if validate_form(form):
+      form = get_data_to_save(form)
+      
+      try:
+        db = get_db()
+        db.execute(
+          """--sql
+          INSERT INTO items (user_id, item_name, amount, measure, quantity_alert, price)
+          VALUES (:user_id, :item_name, :amount, :measure, :quantity_alert, :price);
+          """, form
+        )
+        db.commit()
+        flash("Item created successfully!#success", 'messages')
+        return redirect(url_for("items.overview"))
+      except db.IntegrityError:
+        flash("Item not created. Name must be unique.#danger", 'messages')
+    return render_template("item-detail.html", form=form)
+
+  return render_template("item-detail.html", form=form)
+
+@bp.route("/update-item/<int:id>", methods=["GET", "POST"])
+@login_required
+def update_item(id):
+  """Modify item."""
+  db = get_db()
+  form = get_form_data(id)
+
+  if request.method == "POST":
+    form = get_form(request)
+    
+    if validate_form(form):
+      form = get_data_to_save(form)
+
+      try:
+        db.execute(
+          """--sql
+          UPDATE items SET
+            item_name = :item_name,
+            amount = :amount,
+            measure = :measure,
+            quantity_alert = :quantity_alert,
+            price = :price
+          WHERE id = :id AND user_id = :user_id;
+          """, form
+        )
+        db.commit()
+        flash("Item updated successfully!#success", 'messages')
+        return redirect(url_for("items.overview"))
+      except db.IntegrityError:
+        flash("Item not updated. Name must be unique.#danger", 'messages')
+    return render_template("item-detail.html", form=form)
+
+  return render_template("item-detail.html", form=form)
+
+@bp.route("/delete-item/<int:id>", methods=["POST"])
+@login_required
+def delete_item(id):
+  """Erase item."""
+  db = get_db()
+  item = db.execute(
+    "SELECT * FROM items WHERE id = ? AND user_id = ?;", (id, g.user['id'])
+  ).fetchone()
+
+  if not item:
+    abort(404, description="Item not found or you do not have permission to delete it.")
+
+  db.execute("DELETE FROM items WHERE id = ? AND user_id = ?;", (id, g.user['id']))
+  db.commit()
+  flash("Item deleted successfully!#success", 'messages')
+  return redirect(url_for("items.overview"))
